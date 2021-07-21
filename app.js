6b959f2a3c4cf4a79a7d8ba39e2de160a3246623
jlateef <jlateef808@gmail.com>
2021-07-21 11:28:51 -0400

    set up base server

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 // Set up server
 const http = require("http")
 const express = require("express");
+const db = require('./model/db')
+
+
 //const db = require()
 const app = express();
+
+const server = http.createServer(app)
+
 //include middleware (static files, json, urlencode)
 app.use(express.static("./public"))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log("Server listening on http://127.0.0.1:3000" )
+})
